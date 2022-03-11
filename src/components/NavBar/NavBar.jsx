14e1c54468a20e58c84421f1c568aa5e422e4633
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-03-11 20:37:08 +0300

    corrected navbar.jsx component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { IoMenu } from 'react-icons/io5';
+import { motion, AnimatePresence } from 'framer-motion';
+import useMatchMedia from 'use-match-media-hook';
+
 import Menu from './Menu/Menu';
 
 import s from './NavBar.module.scss';
@@ -20,20 +24,48 @@ const menuList = [
   },
 ];
 
+const queries = ['(max-width: 766px)', '(min-width: 767px)'];
+
+const [mobile] = useMatchMedia(queries);
+const [showMobileMenu, setShowMobileMenu] = useState(false);
+const [isSignupOpen, setSignupOpen] = useState(false);
+const [isLoginOpen, setLoginOpen] = useState(false);
+
+const openSignup = () => setSignupOpen(true);
+const closeSignup = () => setSignupOpen(false);
+const openLogin = () => setLoginOpen(true);
+const closeLogin = () => setLoginOpen(false);
+
 function NavBar() {
   return (
-    <nav className={s.nav}>
-      <div className='container'>
-        <div className={s.logo}>
-          <img className={s.logo} src={logo} alt='logo' />
-        </div>
-        <div className={s.menu}>
-          <Menu links={menuList} handleLogin={openLogin} handleSignup={openSignup} />
-          {/* <Modal />
-          <Modal /> */}
-        </div>
-      </div>
-    </nav>
+    <>
+      <header className={`${s.header} container`}>
+        <img src={logo} alt='logo' className={s.logo} />
+        {mobile ? (
+          <IoMenu className={s.burger} onClick={() => setShowMobileMenu(!showMobileMenu)} />
+        ) : (
+          <Menu links={menuList} handleSignup={openSignup} handleLogin={openLogin} />
+        )}
+        <AnimatePresence>
+          {showMobileMenu && mobile && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: 'auto' }}
+              exit={{ opacity: 0, height: 0 }}
+              className={s.mobileMenu}>
+              <Menu links={menuList} handleSignup={openSignup} handleLogin={openLogin} />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </header>
+
+      <Modal open={isSignupOpen} title='Sign Up' handleClose={closeSignup}>
+        <SignUp closeModal={closeSignup} />
+      </Modal>
+      <Modal open={isLoginOpen} title='Sign In' handleClose={closeLogin}>
+        <Login closeModal={closeLogin} />
+      </Modal>
+    </>
   );
 }
 
