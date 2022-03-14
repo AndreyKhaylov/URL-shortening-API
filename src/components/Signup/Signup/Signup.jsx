60d771e01658f6e3e35b9fef2aba97205f4f3270
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-03-14 14:59:38 +0300

    created signup.jsx component

---
+++ src/components/Signup/Signup/Signup.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import s from './Form.module.scss';
+import { Button } from '../../';
+
+const Form = ({ handleClick, title }) => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+
+  return (
+    <div className={s.form}>
+      <input
+        type='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder='email'
+      />
+      <input
+        type='password'
+        value={pass}
+        onChange={(e) => setPass(e.target.value)}
+        placeholder='password'
+      />
+      <Button onClick={() => handleClick(email, pass)}>{title}</Button>
+    </div>
+  );
+};
+
+export { Form };
